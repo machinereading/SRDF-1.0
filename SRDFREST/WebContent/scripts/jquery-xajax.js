1d8003b217675025a58639a4648a53af2553482e
Sangha Nam <nam.sangha@gmail.com>
2015-11-05 16:25:40 +0900

    Delete Web

--- SRDFREST/WebContent/scripts/jquery-xajax.js
+++
@@ -1,9 +0,0 @@
-/**
-* jQuery.ajax mid - CROSS DOMAIN AJAX
-* ---
-* @author James Padolsey (http://james.padolsey.com)
-* @version 0.11
-* @updated 12-JAN-10
-* @info http://james.padolsey.com/javascript/cross-domain-requests-with-jquery/
-*/
-jQuery.xajax=function(d){function e(a){return!f.test(a)&&/:\/\//.test(a)}var c=location.protocol,f=RegExp(c+"//"+location.hostname),g="http"+(/^https/.test(c)?"s":"")+"://query.yahooapis.com/v1/public/yql?callback=?";return function(a){var b=a.url;/get/i.test(a.type)&&!/json/i.test(a.dataType)&&e(b)&&(a.url=g,a.dataType="json",a.data={q:'select * from html where url="{URL}" and xpath="*"'.replace("{URL}",b+(a.data?(/\?/.test(b)?"&":"?")+jQuery.param(a.data):"")),format:"xml"},!a.success&&a.complete&& (a.success=a.complete,delete a.complete),a.success=function(a){return function(b){a&&a.call(this,{responseText:(b.results[0]||"").replace(/<script[^>]+?\/>|<script(.|\s)*?\/script>/gi,"")},"success")}}(a.success));return d.apply(this,arguments)}}(jQuery.ajax);
--- SRDFREST/src/main/java/edu/kaist/cs/tools/ETRIExecutor.java
+++ SRDFREST/src/main/java/edu/kaist/cs/tools/ETRIExecutor.java
@@ -2,13 +2,10 @@ package main.java.edu.kaist.cs.tools;
 
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.util.ArrayList;
